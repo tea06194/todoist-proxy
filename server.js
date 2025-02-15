a49c8278d0a6b3266ae36bd32380e9e3a91301eb
tea06194 <tea06194@gmail.com>
2025-02-15 22:20:42 +0300

    fix urls

--- server.js
+++ server.js
@@ -2,41 +2,41 @@ import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
 
-dotenv.config(); // Загружаем переменные окружения
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// Токен и URL API
-const TODOIST_API_URL = "https://api.todoist.com/sync/v9/sync"; // Базовый URL для синхронизации
-const TODOIST_TOKEN = process.env.TODOIST_API_TOKEN; // Токен для авторизации
+// Конфиг API
+const TODOIST_API_URL = "https://api.todoist.com";
+const TODOIST_TOKEN = process.env.TODOIST_API_TOKEN;
 
-// Прокси-сервер для передачи запросов в Todoist API
-app.post("/api/todoist/*", async (req, res) => {
-  const endpoint = req.url.replace('/api/todoist', ''); // Получаем endpoint из URL запроса
-  console.log("start req");
+// Прокси сервер
+app.all("/*", async (req, res) => {
+  const apiUrl = `${TODOIST_API_URL}${req.originalUrl}`;
+
   try {
-    // Формируем запрос в Todoist API с добавлением токена в заголовки
-    const response = await fetch(`${TODOIST_API_URL}${endpoint}`, {
-      method: req.method, // Используем метод запроса (GET, POST, и т.д.)
+    const response = await fetch(apiUrl, {
+      method: req.method,
       headers: {
         Authorization: `Bearer ${TODOIST_TOKEN}`,
         "Content-Type": "application/json",
       },
-      body: req.method === "POST" ? JSON.stringify(req.body) : undefined, // Если это POST, передаем тело запроса
+      body:
+        req.method === "POST" || req.method === "PUT"
+          ? JSON.stringify(req.body)
+          : undefined,
     });
-    console.log("req su");
-    // Если запрос успешен, отправляем ответ клиенту
+
     const data = await response.json();
-    res.json(data);
+    res.status(response.status).json(data);
   } catch (error) {
-    console.error("Error proxying request to Todoist:", error);
-    res.status(500).json({ error: "Failed to proxy request to Todoist" });
+    console.error("Ошибка при проксировании запроса:", error);
+    res.status(500).json({ error: "Ошибка на сервере прокси" });
   }
 });
 
-// Запуск сервера
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Прокси работает на порту ${PORT}`);
 });
